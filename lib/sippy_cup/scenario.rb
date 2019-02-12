61efd86ac8323cdc586a7bf0318d9d88fe93e9c6
Nikolas Nisidis <nnisidis@omilia.com>
2019-02-12 11:56:28 +0200

    Invite via proxy

--- lib/sippy_cup/scenario.rb
+++ lib/sippy_cup/scenario.rb
@@ -220,6 +220,59 @@ a=fmtp:101 0-15
       @reference_variables += %w(remote_addr local_addr call_addr)
     end
 
+    def invite_via_proxy(invite_headers, opts = {})
+      opts[:retrans] ||= 500
+      # FIXME: The DTMF mapping (101) is hard-coded. It would be better if we could
+      # get this from the DTMF payload generator
+      from_domain = @from_domain || @adv_ip
+      from_addr = "#{@from_user}@#{from_domain}:[local_port]"
+      max_forwards = opts[:max_forwards] || 100
+      user_agent = opts[:user_agent].present? ? opts[:user_agent]: USER_AGENT
+      msg = <<-MSG
+
+INVITE sip:#{to_addr} SIP/2.0
+Via: SIP/2.0/[transport] #{@adv_ip}:[local_port];branch=[branch]
+From: "#{@from_user}" <sip:#{from_addr}>;tag=[call_number]
+To: <sip:#{to_addr}>
+Call-ID: [call_id]
+CSeq: [cseq] INVITE
+Contact: <sip:#{@from_user}@#{@adv_ip}:[local_port];transport=[transport]>
+Max-Forwards: #{max_forwards}
+User-Agent: #{user_agent}
+Content-Type: application/sdp
+Content-Length: [len]
+#{invite_headers}
+
+v=0
+o=user1 53655765 2353687637 IN IP[local_ip_type] #{@adv_ip}
+s=-
+c=IN IP[media_ip_type] [media_ip]
+t=0 0
+m=audio [auto_media_port] RTP/AVP 0 101
+a=rtpmap:0 PCMU/8000
+a=rtpmap:101 telephone-event/8000
+a=fmtp:101 0-15
+      MSG
+      send msg, opts do |send|
+        send << doc.create_element('action') do |action|
+          action << doc.create_element('assignstr') do |assignstr|
+            assignstr['assign_to'] = "remote_addr"
+            assignstr['value']     = to_addr
+          end
+          action << doc.create_element('assignstr') do |assignstr|
+            assignstr['assign_to'] = "local_addr"
+            assignstr['value']     = from_addr
+          end
+          action << doc.create_element('assignstr') do |assignstr|
+            assignstr['assign_to'] = "call_addr"
+            assignstr['value']     = to_addr
+          end
+        end
+      end
+      # These variables will only be used if we initiate a hangup
+      @reference_variables += %w(remote_addr local_addr call_addr)
+    end
+
     def subscribe(opts = {})
       msg = <<-MSG
 SUBSCRIBE sip:#{@from_user}@#{@adv_ip} SIP/2.0
